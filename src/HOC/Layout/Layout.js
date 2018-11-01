bf696c1e1933aedab3e0ddf35e705f528db08335
Alex_Sandok <alex.pro.sandok@gmail.com>
2018-11-01 14:42:55 +0200

    creating starting template for app

---
+++ src/HOC/Layout/Layout.js
@@ -0,0 +1,31 @@
+// react
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+// styles
+import './Layout.scss'
+import img from '../../assets/images/coin.jpeg'
+
+// semantic-ui
+import { Image } from 'semantic-ui-react'
+
+export default class Layout extends Component {
+  render() {
+    return (
+      <div className="layout">
+        <header className="layout-header">
+          <p>Coins Table</p>
+          <div>
+            <Image src={img} alt="image" width={50} height={50} />
+          </div>
+        </header>
+        <section className="layout-content">{this.props.children}</section>
+        <footer className="layout-footer">&copy; Alex_Sandok 2018</footer>
+      </div>
+    )
+  }
+}
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
---
+++ src/index.js
@@ -0,0 +1,32 @@
+// react
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+// styles
+import './index.css'
+import 'semantic-ui-css/semantic.min.css'
+
+// components
+import App from './containers/App'
+
+// redux
+import { createStore, applyMiddleware, compose } from 'redux'
+import thunk from 'redux-thunk'
+import { logger } from 'redux-logger'
+import { Provider } from 'react-redux'
+import reducers from './reducers/index'
+
+const middleware = [thunk]
+const devMiddleware = [logger]
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const store = createStore(
+  reducers,
+  composeEnhancers(applyMiddleware(...[...middleware, ...devMiddleware])),
+)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+)
